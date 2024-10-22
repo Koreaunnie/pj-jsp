dca287607c28b7f14b6b5ce1292086e42d591087
koreaunnie <hyunjung1995@gmail.com>
2024-10-22 09:26:43 +0900

    2024.10.22 최초 커밋

---
+++ src/main/java/com/example/pjjsp241022/PjJsp241022Application.java
@@ -0,0 +1,13 @@
+package com.example.pjjsp241022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjJsp241022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjJsp241022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pjjsp241022/PjJsp241022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pjjsp241022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjJsp241022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
